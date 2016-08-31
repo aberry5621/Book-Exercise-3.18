990ed69c5500933f7a3d4f1cbeead80450f77856
alexberry721 <alexberry721@gmail.com>
2016-08-31 16:09:38 -0700

    Add three numbers, easy peasy?

--- Book Exercise 3.18 - Add 3 Numbers/main.cpp
+++ Book Exercise 3.18 - Add 3 Numbers/main.cpp
@@ -1,15 +1,45 @@
 //
 //  main.cpp
-//  Book Exercise 3.18 - Add 3 Numbers
+//  Book Exercise 3.18
 //
-//  Created by ax on 8/31/16.
+//  Created by ax on 8/30/16.
 //  Copyright © 2016 COMP130. All rights reserved.
+//
+//  Book Exercise 3.18 - Addition of 3 Numbers Quiz
 //
 
 #include <iostream>
+#include <ctime> // for time function
+#include <cstdlib> // for rand and srand functions
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    // Title
+    cout << "Addition Quiz - 3 Numbers \n";
+    
+    // D
+    int number1 = 0;
+    int number2 = 0;
+    int number3 = 0;
+    int answer = 0;
+    
+    // P
+    // Generate two random digits
+    srand(time(0)); // rand seed
+    number1 = rand() % 100;
+    number2 = rand() % 100;
+    number3 = rand() % 100;
+    
+    // O
+    cout << "What is " << number1 << " + " << number2 << " + " << number3 << "? ";
+    cin >> answer;
+    
+    if (number1 + number2 + number3 == answer) {
+        cout << "Weeee've got a winner! You are correct!" << endl;
+    } else {
+        cout << "You're wrong and should feel bad. " << number1 << " + " << number2 << " + " << number3 << " should be " << (number1 + number2 + number3) << ". Even babies know that." << endl;
+    }
+    
     return 0;
+    
 }
